697c81e24994ef1408a2179363236d8f8344cd95
yuiseki <yuiseki@gmail.com>
2023-02-14 18:53:51 +0900

    update RSpec for tweet

--- spec/simple_tweet_spec.rb
+++ spec/simple_tweet_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SimpleTweet do
     expect(SimpleTweet::VERSION).not_to be nil
   end
 
-  describe "tweet text" do
+  describe ".tweet" do
     let(:twitter_client) do
       SimpleTweet::Client.new(
         consumer_key: "twitter_consumer_key",
@@ -15,23 +15,34 @@ RSpec.describe SimpleTweet do
       )
     end
     let(:message) { "tweet!!" }
-    let!(:stub_tweet_request) do
-      stub_request(
-        :post,
-        SimpleTweet::Client::TW_API_ORIGIN + "/1.1/statuses/update.json?status=#{::CGI.escape(message)}"
-      ).to_return(
-        body: { text: message, created_at: Time.now, user: {} }.to_json,
-        status: 200,
-        headers: { "Content-Type" => "application/json" }
-      )
-    end
 
-    subject do
-      twitter_client.tweet(message: message)
-    end
+    context "tweet only message" do
+      let!(:stub_tweet_request) do
+        stub_request(
+          :post,
+          SimpleTweet::Client::TW_API_ORIGIN + "/1.1/statuses/update.json?status=#{::CGI.escape(message)}"
+        ).to_return(
+          body: { text: message, created_at: Time.now, user: {} }.to_json,
+          status: 200,
+          headers: { "Content-Type" => "application/json" }
+        )
+      end
 
-    it "post tweet request" do
-      expect(subject.text).to eq(message)
+      subject do
+        twitter_client.tweet(message: message)
+      end
+
+      before do
+        subject
+      end
+
+      it "post API request" do
+        expect(stub_tweet_request).to have_been_requested
+      end
+
+      it "get API response" do
+        expect(subject.text).to eq(message)
+      end
     end
   end
 end
