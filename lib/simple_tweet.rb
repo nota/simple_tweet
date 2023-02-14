a23c7bf8f7f89a2631368f4092d0159db45281e8
Kugayama Nana <nonamea.774@gmail.com>
2023-02-14 14:30:16 +0900

    initial commit

---
+++ lib/simple_tweet.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "simple_tweet/version"
+
+module SimpleTweet
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/simple_tweet/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SimpleTweet
+  VERSION = "0.1.0"
+end
