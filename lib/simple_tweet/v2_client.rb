3b414558d78fe87a7c0be156d4d21a84a98de392
Kugayama Nana <nonamea.774@gmail.com>
2023-09-01 17:41:11 +0900

    impl alt text

--- lib/simple_tweet/v2_client.rb
+++ lib/simple_tweet/v2_client.rb
@@ -7,6 +7,7 @@ module SimpleTweet
     # mediaのuploadはapi 1.1しか用意されていないため、それを使う。
     class Client < V1::Client
       TW_TWEET_PATH = "/2/tweets"
+      TW_METADATA_CREATE_PATH = "/1.1/media/metadata/create.json"
       UA = "SimpleTweet/#{SimpleTweet::VERSION}".freeze
 
       def tweet(message:, media_ids: [])
@@ -14,6 +15,27 @@ module SimpleTweet
         json[:media] = { media_ids: media_ids } unless media_ids.empty?
         header = { "User-Agent": UA, "content-type": "application/json" }
         access_token.post(TW_TWEET_PATH, json.to_json, header)
+      end
+
+      def tweet_with_media(message:, media_type:, media:, alt_text: nil)
+        media_ids = upload_media(media_type: media_type, media: media)
+        unless alt_text.nil?
+          media_ids.each do |media_id|
+            create_media_metadata(media_id: media_id, alt_text: alt_text)
+          end
+        end
+        tweet(message: message, media_ids: media_ids)
+      end
+
+      private
+
+      def create_media_metadata(media_id:, alt_text:)
+        header = { "content-type": "application/json; charset=UTF-8" }
+        req = ::Net::HTTP::Post.new(TW_METADATA_CREATE_PATH, header)
+        req.body = { media_id: media_id, alt_text: { text: alt_text } }.to_json
+        res = request(req)
+        throw UploadMediaError, "create_media_metadata failed: #{res.code} #{res.body}" if res.code != "200"
+        res
       end
     end
   end
